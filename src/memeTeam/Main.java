0eaa0c1037b92d735bbd4e6715fe5ad4db03326b
Dylan-Kile <dylankile@utexas.edu>
2017-04-27 00:44:09 -0500

    sudoku

--- src/memeTeam/Main.java
+++ src/memeTeam/Main.java
@@ -1,7 +1,11 @@
 package memeTeam;
+import java.util.*;
 import processing.core.PApplet;
 public class Main extends PApplet{
-
+	GUI gui;
+	VisualGrid grid;
+	ArrayList<Button> buttons;
+	String keyDown;
 	public static void main(String[] args) {
 		PApplet.main("memeTeam.Main");
 	}
@@ -11,10 +15,41 @@ public class Main extends PApplet{
 	
 	public void setup() {
 		SudokuPuzzle puzzle = new SudokuPuzzle(0);
-		puzzle.printPuzzle();
+		grid = new VisualGrid(this);
+		gui = new GUI(this,puzzle.getPuzzle());
+		buttons = gui.getButtons();
+		keyDown = "a";
+
 	}
 	public void draw() {
+		background(255);
+		grid.display();		
+		gui.display();
 
 	}
-
+	public void mouseClicked() {
+		for (Button b: buttons) {
+			if (b.mouseHovering()) {
+				b.click();
+			}
+		}
+	}
+	public void keyPressed() {
+		if (isNumeric(Character.toString(key))) {
+			keyDown = Character.toString(key);
+			for (Button b: buttons) {
+				if (b instanceof SudokuButton && b.isClicked()) {
+					b.giveValue(Integer.parseInt(keyDown));
+					System.out.println(keyDown);
+				}
+			}
+		}
+	}
+	public void keyReleased() {
+		keyDown = "a";
+	}
+	public static boolean isNumeric(String s) {
+		return s.matches("-?\\d+(\\.\\d+)?");
+	}
+	
 }
